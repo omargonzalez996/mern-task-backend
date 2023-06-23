0fac9c926cb0fc6dc9d982397b681326ac408e87
omargonzalez996 <omar.gonz996@gmail.com>
2023-06-23 13:25:37 -0600

    on Submit func for task registration

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,16 +1,44 @@
 import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 function TasksPage() {
-    const { register, handleSubmit } = useForm()
+    const { register, setValue, handleSubmit } = useForm()
+    const navigate = useNavigate()
+
+    const onSubmit = async (data) => {
+        try {
+            if (params.id) {
+                updateTask(params.id, {
+                    ...data,
+                    date: dayjs.utc(data.date).format(),
+                });
+            } else {
+                createTask({
+                    ...data,
+                    date: dayjs.utc(data.date).format(),
+                });
+            }
+
+            // navigate("/tasks");
+        } catch (error) {
+            console.log(error);
+            // window.location.href = "/";
+        }
+    };
+
     return (
         <div>
             <form action="">
+
                 <input type="text" placeholder="Title"
                     {...register('title')}
                     autoFocus />
+
                 <textarea rows="3" placeholder="Description"></textarea>
+
                 <button>
                     Save
                 </button>
+
             </form>
         </div>
     )
